f94f8800660321cff3e35751695fcd06b6ebaa96
Nitish Kumar <nitishkumar@Nitishs-MacBook-Pro.local>
2020-03-23 13:17:08 +0530

    admin

--- resources/views/layouts/subadmin/footer.blade.php
+++ resources/views/layouts/subadmin/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- footer content -->
 <footer>
     <div class="pull-right">
-        Sanjeevani <a href="javaScript:void(0);">@</a>
+        Bajrang Transport <a href="javaScript:void(0);">@</a>
     </div>
     <div class="clearfix"></div>
 </footer>
