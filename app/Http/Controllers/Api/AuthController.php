bb9b3dc4b555cf48f6984ce45116f30b7215f17e
nitish <nitish@rizilianttech.com>
2020-10-09 11:52:09 +0530

    'admin'

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,82 +12,6 @@ use App\Models\Trip;
 
 class AuthController extends Controller {
 
-       /**
-     * @api {post} /api/send-otp  Send OTP
-     * @apiHeader {String} Accept application/json. 
-     * @apiName PostSendOtp
-     * @apiGroup Auth
-     * 
-     * @apiParam {String} mobile_number User unique mobile number*.
-     * @apiParam {String} user_type User type*. (User => 2).
-     * 
-     * @apiSuccess {String} success true 
-     * @apiSuccess {String} status_code (200 => success, 404 => Not found or failed). 
-     * @apiSuccess {String} message OTP sent successfully.
-     * @apiSuccess {JSON} data blank object.
-     * 
-     * @apiSuccessExample {json} Success-Response:
-     * HTTP/1.1 200 OK
-     *   {
-     *       "status": true,
-     *       "status_code": 200,
-     *       "message": "OTP sent successfully.",
-     *       "data": {}
-     *   }
-     * 
-     * @apiError MobileNumberMissing The mobile number is missing.
-     * @apiErrorExample Error-Response:
-     * HTTP/1.1 404 Not Found
-     *   {
-     *       "status": false,
-     *       "status_code": 404,
-     *       "message": "Mobile number missing.",
-     *       "data": {}
-     *   }
-     * 
-     * @apiError UserTypeMissing The User type is missing.
-     * @apiErrorExample Error-Response:
-     * HTTP/1.1 404 Not Found
-     *  {
-     *     "status": false,
-     *     "status_code": 404,
-     *     "message": "User type missing.",
-     *     "data": {}
-     *  }
-     * 
-     *  
-     */
-    public function sendOTP(Request $request) {
-        if (!$request->mobile_number) {
-            return $this->errorResponse("Mobile number missing");
-        }
-        if (!$request->user_type) {
-            return $this->errorResponse("User type missing");
-        }
-        if (!(in_array($request->user_type, [2]))) {
-            return $this->sendErrorResponse("User type invalid.", (object) []);
-        }
-        if (strlen($request->mobile_number) != 10) {
-            return $this->sendErrorResponse("Mobile number should be 10 digit.", (object) []);
-        }
-        $otp = 1234;
-        if ($request->user_type == 2) {
-            $existingUser = User::where(['user_type_id' => $request->user_type, 'mobile_number' => $request->mobile_number])->first();
-            if ($existingUser) {
-                $existingUser->otp = $otp;
-                if ($existingUser->save()) {
-                    // try {
-                    //     $this->sendOTPMobile($request->mobile_number, $otp, "KdL20Pu35u2");
-                    // } catch (Exception $ex) {
-                        
-                    // }
-                    return $this->successResponse("OTP sent successfully.", (object) []);
-                }
-            } else {
-                return $this->errorResponse("You have not registered with us.");
-            }
-        }
-    }
     /**
      * @api {post} /api/verify-otp  Verify OTP
      * @apiHeader {String} Accept application/json.
@@ -97,8 +21,8 @@ class AuthController extends Controller {
      * @apiParam {String} mobile_number Users mobile number*.
      * @apiParam {String} otp OTP*.
      * @apiParam {String} user_type User type*. (Staff member => 2 ).
-     * @apiParam {String} longitude cordinate location*.
-     * @apiParam {String} latitude cordinate location*.
+     * @apiParam {String} longitude cordinate location.
+     * @apiParam {String} latitude cordinate location.
      * 
      *
      * @apiSuccess {String} success true 
@@ -209,17 +133,11 @@ class AuthController extends Controller {
             if (!$request->mobile_number) {
                 return $this->sendErrorResponse("Mobile number missing.", (object) []);
             }
-            if (!$request->longitude) {
-                return $this->sendErrorResponse("longitude missing.", (object) []);
-            }
-            if (!$request->latitude) {
-                return $this->sendErrorResponse("Latitude missing.", (object) []);
-            }
             if (strlen($request->mobile_number) != 10) {
                 return $this->sendErrorResponse("Mobile number should be 10 digit.", (object) []);
             }
-            if (!$request->otp) {
-                return $this->sendErrorResponse("OTP missing.", (object) []);
+            if (!$request->password) {
+                return $this->sendErrorResponse("password missing.", (object) []);
             }
             if (!$request->user_type) {
                 return $this->sendErrorResponse("User type missing.", (object) []);
@@ -231,17 +149,17 @@ class AuthController extends Controller {
             $user = User::Where([
                 'user_type_id' => $request->user_type,
                 'mobile_number' => $request->mobile_number,
-                'otp' => $request->otp
+                'password' => $request->password
             ])->first();
             if (!$user) {
-                return $this->sendErrorResponse("OTP or mobile number incorrect.", (object) []);
+                return $this->sendErrorResponse("Password or mobile number incorrect.", (object) []);
             }
 
             if ($user->is_active == 0) {
                 return $this->sendInactiveAccountResponse();
             }
 
-            return $this->sendSuccessResponse("OTP verified successfully.", $user);
+            return $this->sendSuccessResponse("Password verified successfully.", $user);
         } catch (\Exception $e) {
             return $this->sendErrorResponse($e->getMessage(), (object) []);
         }
@@ -273,8 +191,6 @@ class AuthController extends Controller {
         if ($user) {
             if ($user->user_type_id == 2) {
                 $user->device_token = NULL;
-                $user->device_type = NULL;
-                $user->device_id = NULL;
                 $user->save();
             }
         }
--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-10-09T06:07:32.600Z",
+    "time": "2020-10-09T06:21:57.649Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
