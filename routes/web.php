ea6019ae5d29f00b91be884258ecb877f52ad901
nitish <nitish@rizilianttech.com>
2020-10-12 12:22:35 +0530

    'admin'

--- routes/web.php
+++ routes/web.php
@@ -249,6 +249,7 @@ Route::namespace("SubAdmin")->prefix('sub-admin')->middleware(['subadminGuest'])
         Route::match(['get', 'post'], '/add-trip', 'TripController@addTrip')->name('subadmin.trip.add');
         Route::match(['get', 'post'], '/fuel/{id}', 'TripController@fuelTrip')->name('subadmin.trip.fuel');
         Route::match(['get', 'post'], '/edit/{id}', 'TripController@editTrip')->name('subadmin.trip.edit');
+        Route::match(['get', 'post'], '/view/{id}', 'TripController@viewTrip')->name('subadmin.trip.view');
     });
 
       /**
