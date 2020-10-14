e52e1e6e43205b8eca8cd0de66f67717dc89c8cb
nitish <nitish3939@gmail.com>
2020-10-14 13:39:16 +0530

    'admin'

--- app/Http/Controllers/SubAdmin/StaffController.php
+++ app/Http/Controllers/SubAdmin/StaffController.php
@@ -70,7 +70,8 @@ class StaffController extends Controller {
                 $usersArray[$i]['mobileno'] = $user->mobile_number;
                 $checked_status = $user->is_active ? "checked" : '';
                 $usersArray[$i]['status'] = "<label class='switch'><input  type='checkbox' class='user_status' id=" . $user->id . " data-status=" . $user->is_active . " " . $checked_status . "><span class='slider round'></span></label>";
-                $usersArray[$i]['view-deatil'] = '<a class="btn btn-info btn-xs" href="' . route('subadmin.staff.edit', ['id' => $user->id]) . '"><i class="fa fa-pencil"></i>Edit</a>';
+                $usersArray[$i]['view-deatil'] = '<a class="btn btn-info btn-xs" href="' . route('subadmin.staff.edit', ['id' => $user->id]) . '"><i class="fa fa-pencil"></i>Edit</a>'
+                . '<a class="btn btn-primary btn-xs" href="' . route('subadmin.staff.change-password', ['id' => $user->id]) . '"><i class="fa fa-pencil"></i>Change Password</a>';
                 $i++;
             }
 
