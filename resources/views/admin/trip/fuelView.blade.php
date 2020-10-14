90cdd9d241e5bc87387f2690e1dfd0c89b3103fe
nitish <nitish3939@gmail.com>
2020-10-14 14:25:44 +0530

    'admin'

--- resources/views/admin/trip/fuelView.blade.php
+++ resources/views/admin/trip/fuelView.blade.php
@@ -71,7 +71,7 @@
                     <div class="ln_solid"></div>
                     <div class="form-group">
                         <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
-                            <a  class="btn btn-default" href="{{ route('admin.trip.fuel',$fuel->id) }}">Cancel</a>
+                            <a  class="btn btn-default" href="{{ route('admin.trip.fuel',$fuel->trip_id) }}">Cancel</a>
                             <button type="submit" class="btn btn-success">Update</button>
                         </div>
                     </div>
--- resources/views/subadmin/vehicle/issueView.blade.php
+++ resources/views/subadmin/vehicle/issueView.blade.php
@@ -132,7 +132,7 @@
                     <div class="ln_solid"></div>
                     <div class="form-group">
                         <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
-                            <a  class="btn btn-default" href="{{ route('subadmin.vehicle.issueView',$vehicle->id) }}">Cancel</a>
+                            <a  class="btn btn-default" href="{{ route('subadmin.vehicle.issue',$vehicle->id) }}">Cancel</a>
                             <button type="submit" class="btn btn-success">Update</button>
                         </div>
                     </div>
